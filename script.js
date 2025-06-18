7e680fe17e765e1944141cbd5129e3fc278e90be
Raghul M <raghulsoftwaredev@gmail.com>
2025-06-18 12:51:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
-$( document ).ready(function() {
-    
+$(document).ready(function () {
     var envelope = $('#envelope');
     var btn_open = $("#open");
     var btn_reset = $("#reset");
-    
-    envelope.click( function() {
-        open();
-    });
-    btn_open.click( function() {
-        open();
-    });
-    btn_reset.click( function() {
-        close();
-    });
+    var letter = $(".letter");
+    var popup = $("#letterPopup");
 
-    function open() {
-        envelope.addClass("open")
-           .removeClass("close");
+    function openEnvelope() {
+        envelope.addClass("open").removeClass("close");
+
+        setTimeout(() => {
+            letter.addClass("shake");
+
+            setTimeout(() => {
+                letter.removeClass("shake");
+                popup.removeClass("hidden");
+            }, 1000); // After shake
+        }, 2000); // Delay after open
     }
-    function close() {
-        envelope.addClass("close")
-           .removeClass("open");
+
+    function closeEnvelope() {
+        envelope.addClass("close").removeClass("open");
+        popup.addClass("hidden");
     }
-   
+
+    envelope.click(openEnvelope);
+    btn_open.click(openEnvelope);
+    btn_reset.click(closeEnvelope);
+
+    $(document).click(function (e) {
+        if (!$(e.target).closest('.popup-content, #open, #reset, #envelope').length) {
+            popup.addClass("hidden");
+        }
+    });
 });
